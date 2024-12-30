74bda85ed40b2e251cb9ae9b17f36cee3fffd113
Ankush Kumar <imankush1010@gmail.com>
2024-12-30 22:59:17 +0530

    Tourist app initialized

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-3xl text-red-600">GoodBye World!</Text>
+    </View>
+  );
+}
